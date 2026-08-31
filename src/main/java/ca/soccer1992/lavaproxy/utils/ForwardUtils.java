93e082b8e78bb7b1ff61648a32520c26dff0df0d
soccer1992 <soccer1992.is.a.dev@gmail.com>
2026-08-31 14:09:59 -0400

    WHY DID THEY DESIGN IT SO ITS A LIST AND NOT A KEYVALUE

--- src/main/java/ca/soccer1992/lavaproxy/utils/ForwardUtils.java
+++ src/main/java/ca/soccer1992/lavaproxy/utils/ForwardUtils.java
@@ -2,7 +2,11 @@ package ca.soccer1992.lavaproxy.utils;
 
 import ca.soccer1992.lavaproxy.Connection;
 import ca.soccer1992.lavaproxy.Player;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import java.util.Map;
 
 public class ForwardUtils {
   public static String buildBungeeCordData(Player player, JsonObject properties) {
@@ -14,7 +18,23 @@ public class ForwardUtils {
             + '\0'
             + player.uuid.toString().replace("-", "");
 
-    if (properties != null) data += '\0' + properties.toString();
+    if (properties != null) {
+      data += '\0';
+      JsonArray propertiesArray = new JsonArray();
+
+      // 2. Loop through every key-value pair inside your x:y object
+      for (Map.Entry<String, JsonElement> entry : properties.entrySet()) {
+        JsonObject propertyPair = new JsonObject();
+
+        // Map the key to "name" and the value to "value"
+        propertyPair.addProperty("name", entry.getKey());
+        propertyPair.addProperty("value", entry.getValue().getAsString());
+
+        // Add this pair to the main array
+        propertiesArray.add(propertyPair);
+      }
+      data += propertiesArray.toString();
+    }
 
     return data;
   }
